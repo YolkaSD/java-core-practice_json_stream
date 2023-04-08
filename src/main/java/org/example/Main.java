a1fce0a75a66b8bc61e0f447c6bf5da5cfc9a474
Slava Rybkin <yolka.sd@gmail.com>
2023-04-08 16:49:33 +0700

    practic 9 v2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,33 +11,33 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         ObjectMapper mapper = new ObjectMapper(); //.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        Consumer consumer = mapper.readValue(new File("src/main/resources/bank_data.json"), new TypeReference<>() {});
+        Consumer consumerData = mapper.readValue(new File("src/main/resources/bank_data.json"), new TypeReference<>() {});
         //Task - 1
-        System.out.println(notNullCountDelis(consumer));
+        System.out.println(countNonNullCreditLimits(consumerData, 0));
 
         //Task - 2
-        System.out.println(maxBalance(consumer));
+        System.out.println(maxBalanceAmount(consumerData));
 
         //Task - 3
-        System.out.println(getMaxBalanceDateBefore(consumer, 18));
+        System.out.println(getMaxCreditLimitLastMonths(consumerData, 18));
 
         //Task - 4
-        System.out.println(getDelays(consumer, 24));
+        System.out.println(getDelays(consumerData, 24));
 
     }
 
-    private static int notNullCountDelis(Consumer consumer) {
-        return (int) consumer.getConsumerData().getCais().stream()
+    private static int countNonNullCreditLimits(Consumer consumerData, int minCreditLimit) {
+        return (int) consumerData.getConsumerData().getCais().stream()
                 .map(CAIS::getCAISDetails)
                 .flatMap(Collection::stream)
                 .map(CAISDetails::getCreditLimit)
                 .filter(Objects::nonNull)
                 .mapToInt(creditLimit -> parseBalance(creditLimit.getAmount()))
-                .filter(limit -> limit > 0)
+                .filter(limit -> limit > minCreditLimit)
                 .count();
     }
-    private static int maxBalance(Consumer consumer) {
-        OptionalInt max = consumer.getConsumerData().getCais().stream()
+    private static int maxBalanceAmount(Consumer consumerData) {
+        return consumerData.getConsumerData().getCais().stream()
                 .map(CAIS::getCAISDetails)
                 .flatMap(Collection::stream)
                 .map(CAISDetails::getBalance)
@@ -45,38 +45,42 @@ public class Main {
                 .map(Balance::getAmount)
                 .filter(Objects::nonNull)
                 .mapToInt(Main::parseBalance)
-                .max();
-        return max.isPresent() ? max.getAsInt() : 0;
+                .max()
+                .orElse(0);
     }
-    private static int getMaxBalanceDateBefore(Consumer consumer, int months) {
-        OptionalInt max = consumer.getConsumerData().getCais().stream()
+    private static int getMaxCreditLimitLastMonths(Consumer consumerData, int months) {
+        LocalDate cutoffDate = LocalDate.now().minusMonths(months);
+        return consumerData.getConsumerData().getCais().stream()
                 .map(CAIS::getCAISDetails)
                 .flatMap(Collection::stream)
                 .filter(caisDetails -> caisDetails.getCAISAccSwtartDate()
-                        .isAfter(LocalDate.now().minusMonths(months)))
+                        .isAfter(cutoffDate))
                 .map(CAISDetails::getCreditLimit)
                 .filter(Objects::nonNull)
                 .map(CreditLimit::getAmount)
                 .mapToInt(Main::parseBalance)
-                .max();
-        return max.isPresent() ? max.getAsInt() : 0;
+                .max()
+                .orElse(0);
     }
-    private static int getDelays(Consumer consumer, int month) {
-        return (int) consumer.getConsumerData().getCais().stream()
+    private static int getDelays(Consumer consumerData, int month) {
+        return (int) consumerData.getConsumerData().getCais().stream()
                 .map(CAIS::getCAISDetails)
                 .flatMap(Collection::stream)
                 .filter(caisDetails -> caisDetails.getCAISAccSwtartDate().isAfter(LocalDate.now().minusMonths(24)))
-                .flatMap(caisDetails ->  caisDetails.getAccountBalances().stream()
+                .flatMap(caisDetails ->   {
+                    int limit = getMonthsOverlap(caisDetails.getCAISAccSwtartDate(), caisDetails.getLastUpdatedDate(),
+                            LocalDate.now(), LocalDate.now().minusMonths(month));
+                    return caisDetails.getAccountBalances().stream()
                             .map(AccountBalances::getStatus)
-                            .limit(findCommonMonths(caisDetails.getCAISAccSwtartDate(), caisDetails.getLastUpdatedDate(),
-                                    LocalDate.now(), LocalDate.now().minusMonths(month))))
+                            .limit(limit);
+                })
                 .filter(s -> !s.equals("U") && !s.equals("0"))
                 .count();
     }
     private static int parseBalance(String balanceAmount) {
         return Integer.parseInt(balanceAmount.replaceAll("£", ""));
     }
-    private static int findCommonMonths(LocalDate p1Date1, LocalDate p1Date2, LocalDate p2Date1, LocalDate p2Date2) {
+    private static int getMonthsOverlap(LocalDate p1Date1, LocalDate p1Date2, LocalDate p2Date1, LocalDate p2Date2) {
         LocalDate pStart1 = p1Date1;
         LocalDate pEnd1 = p1Date2;
         LocalDate pStart2 = p2Date1;
@@ -94,9 +98,9 @@ public class Main {
         if (pEnd1.isBefore(pStart2) || pEnd2.isBefore(pStart1)) {
            return 0;
         }
+        int MONTHS_INCLUSIVE = 1;
         LocalDate start = pStart1.isAfter(pStart2) ? pStart1 : pStart2;
         LocalDate end = pEnd1.isAfter(pEnd2) ? pEnd2 : pEnd1;
-        return (int) ChronoUnit.MONTHS.between(start, end) + 1;
-
+        return (int) ChronoUnit.MONTHS.between(start, end) + MONTHS_INCLUSIVE;
     }
 }
